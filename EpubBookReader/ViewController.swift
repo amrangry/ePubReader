ba9c5aaa071e766b4f8f86543dcb17e002a408df
Amr Elghadban <amr.elghadban@gmail.com>
2022-10-13 16:48:41 +0400

    init project

---
+++ EpubBookReader/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  EpubBookReader
+//
+//  Created by Amr Elghadban on 17/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
