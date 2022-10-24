5d864b197a16f0b5fc775dfaad93dd0fac4527e2
Amr Elghadban <amr.elghadban@gmail.com>
2022-10-24 21:04:38 +0400

    integrate   #Readium

--- EpubBookReader/ViewController.swift
+++ EpubBookReader/ViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+//import FolioReaderKit
+import R2Shared
+import R2Streamer
 
 class ViewController: UIViewController {
 
@@ -16,10 +19,40 @@ class ViewController: UIViewController {
 
     @IBAction func openArabicBook(_ sender: Any) {
        print("Arabic")
+        let name = "Arabic_Book"
+        open(book: name)
     }
     
     @IBAction func openEnglishBook(_ sender: Any) {
        print("English")
+       let name = "English_Book"
+       open(book: name)
+    }
+    
+    func open(book name: String) {
+        guard let bookPath = Bundle.main.path(forResource: name, ofType: "epub") else {
+            return
+        }
+//        let config = FolioReaderConfig()
+//        let folioReader = FolioReader()
+//        folioReader.presentReader(parentViewController: self, withEpubPath: bookPath!, andConfig: config)
+        
+        guard let url = URL(string: bookPath) else {
+            return
+        }
+        let streamer = Streamer()
+        streamer.open(asset: FileAsset(url: url), allowUserInteraction: true) { result in
+            switch result {
+            case .success(let publication):
+            print("here")
+            case .failure(let error):
+               // alert(error.localizedDescription)
+                print("here: \(error.localizedDescription)")
+            case .cancelled:
+                break
+            }
+        }
+        
     }
 }
 
