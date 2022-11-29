47cc65bf25fb4522827a15a961d9b50e3a356a3b
Amr Elghadban <amr.elghadban@gmail.com>
2022-11-29 20:35:54 +0400

    install publication

--- ReadiumSDK/EPubReaderConfigurator.swift
+++ ReadiumSDK/EPubReaderConfigurator.swift
@@ -5,30 +5,63 @@
 //  Created by Amr Elghadban on 23/11/2022.
 //
 
-import Foundation
-import UIKit
-
-//import Combine
+//import Foundation
 //import UIKit
-//import MobileCoreServices
-//import WebKit
-//import UniformTypeIdentifiers
-//import R2Shared
-//import R2Streamer
-//import R2Navigator
-//import Kingfisher
+//
+//import Combine
+////import UIKit
+////import MobileCoreServices
+////import WebKit
+////import UniformTypeIdentifiers
+////import R2Shared
+////import R2Streamer
+////import R2Navigator
+////import Kingfisher
+//import ReadiumOPDS
+
+import Combine
+import UIKit
+import MobileCoreServices
+import WebKit
+import R2Shared
+import R2Streamer
+import R2Navigator
+import Kingfisher
 import ReadiumOPDS
+import UniformTypeIdentifiers
 
 class EPubReaderConfigurator {
-    
+
     static let shared = EPubReaderConfigurator()
+    
+    var sender: UIViewController = AppDelegate.shared?.window?.rootViewController ?? UIViewController()
     
     private init() {
         app = try! AppModule()
+        let library = app.library
+        if let libraryModule = library as? LibraryModule {
+            libraryService = libraryModule.library
+            libraryModuleDelegate = libraryModule.delegate
+        }
+        
+        libraryService.allBooks()
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                if case .failure(let error) = completion {
+                    self.libraryModuleDelegate?.presentError(error, from: self.sender)
+                }
+            } receiveValue: { newBooks in
+                self.books = newBooks
+                //self.collectionView.reloadData()
+            }
+            .store(in: &subscriptions)
     }
     
     private (set) var app: AppModule!
-    
+    var libraryService: LibraryService! = nil
+    weak var libraryModuleDelegate: LibraryModuleDelegate?
+    var subscriptions = Set<AnyCancellable>()
+    var books: [Book]?
     // MARK: - Controller
     private var fileName: String?
     func config(_ value: String?) {
@@ -40,46 +73,51 @@ class EPubReaderConfigurator {
     /// Imports a new publication to the library, either from:
     /// - a local file URL
     /// - a remote URL which will be downloaded
-    func instalPublication(url: URL, sender: UIViewController?) {
+    func installPublication(url: URL, sender: UIViewController?) {
 
         func importPublication(from url: URL, sender: UIViewController?) {
             //LibraryModuleAPI
             guard let viewController = AppDelegate.shared?.window?.rootViewController else { return }
-            _ = app.library.importPublication(from: url, sender: viewController)
-            //library.openBook(book, forPresentation: true, sender: self)
+           // _ = app.library.importPublication(from: url, sender: viewController)
+//file:///Users/amr/Library/Developer/CoreSimulator/Devices/168A3DFB-36EF-419B-BD16-EDAC75A4B929/data/Containers/Data/Application/99DF5CC2-9440-416D-ACFD-4142E7F01757/Documents/Inbox/Arabic_Book.epub
             
-            let asset = FileAsset(url: url)
-            guard let mediaType = asset.mediaType() else {
-                promise(.failure(.openFailed(Publication.OpeningError.unsupportedFormat)))
-                return
-            }
-            
-            self.streamer.open(asset: asset, allowUserInteraction: allowUserInteraction, sender: sender) { result in
-                switch result {
-                case .success(let publication):
-                    promise(.success((publication, mediaType)))
-                case .failure(let error):
-                    promise(.failure(.openFailed(error)))
-                case .cancelled:
-                    promise(.failure(.cancelled))
-                }
-            }
+//file:///Users/amr/Library/Developer/CoreSimulator/Devices/168A3DFB-36EF-419B-BD16-EDAC75A4B929/data/Containers/Data/Application/C143555F-0114-4052-A449-2B76620E0614/Documents/FederalistPapers.epub
+            app.library.importPublication(from: url, sender: viewController)
+                .assertNoFailure()
+                .sink { _ in }
+                .store(in: &subscriptions)
+            displayInformation(forCellAt: 0, sender: viewController)
         }
         
         func tryAdd(from url: URL) {
             importPublication(from: url, sender: sender)
         }
         
-        OPDSParser.parseURL(url: url) { data, _ in
-            DispatchQueue.main.async {
-                if let downloadLink = data?.publication?.downloadLinks.first, let downloadURL = URL(string: downloadLink.href) {
-                    tryAdd(from: downloadURL)
-                } else {
-                    tryAdd(from: url)
-                }
-            }
-        }
+        tryAdd(from: url)
         
+    }
+    
+    func displayInformation(forCellAt index: Int, sender: UIViewController) {
+        
+        func done() {
+//                self.loadingIndicator.removeFromSuperview()
+//                collectionView.isUserInteractionEnabled = true
+        }
+        guard let book = books?[index] else { return }
+        print(book)
+        libraryService.openBook(book, forPresentation: true, sender: sender)
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                if case .failure(let error) = completion {
+                    self.libraryModuleDelegate?.presentError(error, from: sender)
+                }
+            } receiveValue: { pub in
+//                let detailsViewController = self.factory.make(publication: pub)
+//                detailsViewController.modalPresentationStyle = .popover
+//                self.navigationController?.pushViewController(detailsViewController, animated: true)
+                self.libraryModuleDelegate?.libraryDidSelectPublication(pub, book: book, completion: done)
+            }
+            .store(in: &subscriptions)
     }
     
     func installPublication(fileName: String) {
