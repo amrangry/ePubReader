eecba3f20ec49217d72d5875ef90f9dc5b4b159d
Amr Elghadban <amr.elghadban@gmail.com>
2023-01-21 14:14:00 +0400

    adding english and arabic book

--- EpubBookReader/ViewController.swift
+++ EpubBookReader/ViewController.swift
@@ -29,51 +29,36 @@ class ViewController: UIViewController {
     
     /*EnglishBook*/
     @IBAction func downloadAndOpenEnglishBook(_ sender: Any) {
-        
-        //        "id": "271961",
-        //        "type": "attachments",
-        //        "attributes": {
-        //            "name": "بلاد تركب العنكبوتepub",
-        //            "file_name": "بلاد-تركب-العنكبوتepub.epub",
-        //            "mime_type": "application/epub+zip",
-        //            "size": "1.44 MB"
-        //        },
-        //        "links": {
-        //            "self": "https://cdn.aseeralkotb.com/storage/media/271961/بلاد-تركب-العنكبوتepub.epub"
-        //        }
-        
-        //        let downloadURL = "http://bbebooksthailand.com/phpscripts/bbdownload.php?ebookdownload=FederalistPapers-EPUB2"
-        //        let fileName = "FederalistPapers.epub"
-        //        let bookTitle = "The Federalist Papers"
-        //        let type = "application/epub+zip"
-        //        let path = "The Federalist Papers.epub"
-        
-        
-        let downloadURLString = "https://cdn.aseeralkotb.com/storage/media/271961/بلاد-تركب-العنكبوتepub.epub"
-        guard let downloadURL = downloadURLString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
-        print(downloadURL)
+        let downloadURLString = "http://bbebooksthailand.com/phpscripts/bbdownload.php?ebookdownload=FederalistPapers-EPUB2"
+        let fileName = "FederalistPapers.epub"
+        let bookTitle = "The Federalist Papers"
+//        let path = "The Federalist Papers.epub"
+        downloadOrRead(fileName: fileName, bookTitle: bookTitle, downloadURLString: downloadURLString)
+    }
+    
+    /*ArabicBook*/
+    @IBAction func downloadAndOpenArabicBook(_ sender: Any) {
+        let downloadURLString = "https://www.adkatech.com/projects/DynamicLink/بلاد-تركب-العنكبوتepub.epub"
         let fileName = "بلاد-تركب-العنكبوتepub.epub"
         let bookTitle = "بلاد تركب العنكبوت"
-        let type = "application/epub+zip"
-        let path = "بلاد-تركب-العنكبوتepub.epub"
-        
-//        - title : "بلاد تركب العنكبوت"
-//        ▿ authors : Optional<String>
-//          - some : "منى سلامة"
-//        - type : "application/epub+zip"
-//        - path : "بلاد تركب العنكبوت.epub"
-//        ▿ coverPath : Optional<String>
-//          - some : "D9132820-BE26-409E-834C-D6951AD266A2"
-//        - locator : nil
-//        - progression : 0.0
-//        ▿ created : ٢٠٢٢-١٢-٠٤ ١٩:٣٧:٥٤ +0000
-//          - timeIntervalSinceReferenceDate : 691875474.774513
-        
+//        let path = "بلاد-تركب-العنكبوتepub.epub"
+        downloadOrRead(fileName: fileName, bookTitle: bookTitle, downloadURLString: downloadURLString)
+    }
+    
+    @IBAction func goToLibrary(_ sender: Any) {
         let ePubReader = EPubReaderConfigurator.shared
+        ePubReader.goToLibrary()
+    }
+    
+}
+
+extension ViewController {
+    
+    func downloadOrRead(fileName: String, bookTitle: String, downloadURLString: String) {
+        let ePubReader = EPubReaderConfigurator.shared
+        guard let downloadURL = downloadURLString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
         let check = ePubReader.isExist(bookTitle, downloadURL: downloadURL)
         if check {
-            print(check)
-            //ePubReader.app.presentAlert("Already Exist", message: "Book", from: self)//presentAlert("Success", message: "Book Downloaded", from: self)
             ePubReader.presentAlertForDownloadedBook("Book Already downloaded into your library!", presenter: self)
         } else {
             // need to download
@@ -87,10 +72,6 @@ class ViewController: UIViewController {
                         case .success(let value):
                             let book = value
                             print(book)
-                            let books = ePubReader.books
-                            let check = ePubReader.isExist(bookTitle, downloadURL: downloadURL)
-                            //print(books)
-                            //ePubReader.app.presentAlert("Success", message: "Book Downloaded", from: self)
                             ePubReader.presentAlertForDownloadedBook("Book downloaded successfully into your library!", presenter: self)
                         case .failure(let error):
                             print(error)
@@ -98,17 +79,9 @@ class ViewController: UIViewController {
                     }
                 case .failure(let error):
                     print(error)
-                    
                 }
-                //                if case .success(let value) = response { }
             }
         }
     }
     
-    @IBAction func goToLibrary(_ sender: Any) {
-        let ePubReader = EPubReaderConfigurator.shared
-        ePubReader.goToLibrary()
-    }
-    
 }
-
